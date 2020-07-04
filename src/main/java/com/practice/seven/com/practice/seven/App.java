f7e140f3db0b80f3c26e03b4b44f02feb9d3b142
Murtaza Akbar <makbar22>
2020-07-03 22:34:35 -0400

    practice seven

---
+++ src/main/java/com/practice/seven/com/practice/seven/App.java
@@ -0,0 +1,13 @@
+package com.practice.seven.com.practice.seven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
